50e9e66de1787b3626bfbefb016947b6c08a489e
IsakLönn <illidan1996@hotmail.com>
2023-11-29 11:56:10 +0100

    lunch commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,22 +1,111 @@
 package se.lexicon;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
+
+    static enum Name
+    {
+        LASTNAME,
+        FIRSTNAME
+    }
+
+    static boolean isRunning = true;
+
     public static void main(String[] args) {
         run();
     }
-
     static void run(){
-        boolean isRunning = true;
+
+        // some variables to use in the switch cases
+        String name;
+        String[] names;
+        boolean success;
 
         while(isRunning)
         {
             welcomeText();
+
+            int value = getIntInput();
+
+            switch (value)
+            {
+                case 1:
+                    name = getFullName();
+                    boolean containsName = NameRepository.add(name);
+                    if(containsName) println("Name already exists. It wont be added to the system");
+                    break;
+
+                case 2:
+                    
+                    break;
+
+                case 3:
+                    //update name
+                    println("Please give the name to update followed by the new name");
+                    name = getFullName();
+                    String updatedName = getFullName();
+                    success = NameRepository.update(name, updatedName);
+                    if(!success) println("Name was not updated. Are you sure you entered the correct name?");
+                    break;
+
+                case 4:
+                    names = NameRepository.findAll();
+                    printArray(names, "All names in the system");
+                    break;
+
+                case 5:
+                    println("There are " + NameRepository.getSize() + " names in the system"); ;
+                    break;
+
+                case 6:
+                    name = getName(true);
+                    names = NameRepository.findByLastName(name);
+                    printArray(names, "Names that go by last name " + name);
+                    break;
+
+                case 7:
+                    name = getName(false);
+                    names = NameRepository.findByFirstName(name);
+                    printArray(names, "Names that go by first name " + name);
+                    break;
+
+                case 8:
+                    println("Are you sure you want to clear all names from the system? y/n");
+                    if(Objects.equals(getInput(false).toLowerCase(), "y")) NameRepository.clear();
+                    break;
+
+                case 9:
+                    name = getFullName();
+                    success = NameRepository.remove(name);
+                    if(!success) println("Error removing name from system. Possible name does not exist?");
+                    break;
+
+                case 10:
+                    println("Exiting program, goodbye.");
+                    isRunning = false;
+                    break;
+            }
+
+
+            getInput(false);
         }
     }
 
     private static void welcomeText() {
         println("welcome to the name repository!");
-        
+        println("Enter what you would like to do:");
+        println("1. Add a name");
+        println("2. Add multiple names NOT IMPLEMENTED");
+        println("3. Update a name in the system");
+        println("4. Get all names in the system");
+        println("5. Get how many names are in the system");
+        println("6. Find by last name");
+        println("7. Find by first name");
+        println("8. Clear all names from the system");
+        println("9. Remove a name from the system");
+        println("10. Exit program");
     }
 
     /**
@@ -32,4 +121,92 @@ public class Main {
     static void println(String text){
         System.out.println(text);
     }
+
+    static int getIntInput()
+    {
+        println("Please input an integer");
+        boolean exception;
+        String input;
+        int value = 0;
+        while(true)
+        {
+            exception = false;
+            input = getInput(false);
+            try {
+            value = Integer.parseInt(input);
+            }catch(Exception e){
+                exception = true;
+            }
+            if(exception) println("Error in input. Please input an integer");
+            else return value;
+        }
+
+    }
+
+    static String getInput(boolean askForInput){
+        if(askForInput) println("Input:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    static String getFullName()
+    {
+        String firstName;
+        String lastName;
+        String fullName = "";
+        boolean correctName = false;
+        while(!correctName)
+        {
+            print("Please give a first name:");
+            firstName = getInput(false);
+            print("Please give a last name:");
+            lastName = getInput(false);
+            fullName = firstName + " " + lastName;
+            println("The name put in is " + fullName + ". Is this correct? y/n");
+
+            correctName = Objects.equals(getInput(false).toLowerCase(), "y");
+        }
+
+        return fullName;
+    }
+
+    /**
+     * gets a name from user, use parameter to tell if it is a first or last name
+     */
+    static String getName(boolean isLastName)
+    {
+        String name = "";
+        boolean correctName = false;
+        while(!correctName)
+        {
+            if(isLastName) print("Please give a last name:");
+            else print("Please give a first name:");
+            name = getInput(false);
+            println("The name put in is " + name + ". Is this correct? y/n");
+
+            correctName = Objects.equals(getInput(false).toLowerCase(), "y");
+        }
+
+        return name;
+    }
+
+    static void printArray(String[] arr, String headerText)
+    {
+        println(headerText);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    static void updatedName(){
+        String originalName = "";
+        String updatedName = "";
+        if (NameRepository.update(originalName, updatedName)) {
+            System.out.println("Updated name: " + originalName + " -> " + updatedName);
+        } else {
+            System.out.println("Name not found or already exists: " + originalName);
+        }
+    }
+
 }
--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,15 +1,16 @@
 package se.lexicon;
+import java.util.Arrays;
 
 public class NameRepository {
 
-    static String[] names;
+    private static String[] names = new String[0];
 
 
     /**
      * returns numbers of elements in the array
      */
     public static int getSize(){
-        return 0;
+        return names.length;
     }
 
     /**
@@ -23,7 +24,7 @@ public class NameRepository {
      * clears all names
      */
     public static void clear(){
-
+        names = new String[0];
     }
 
     /**
@@ -36,34 +37,62 @@ public class NameRepository {
     /**
      * name if found and null if not found.
      */
-    public static String find(final String fullName){
-        return null;
+    public static String[] find(final String fullName){
+
+        return Arrays.copyOf(names, names.length);
     }
 
     /**
      * Should add a new name to the array. Returns
      * true when name was added and false when the array contains the name.
      */
-    public static boolean add(final String fullName){
+    public static boolean add(final String fullName) {
+        if (find(fullName) != null) {
+            return false;
+        }
+        for (int i = 0; i < names.length; i++) {
+            if(names[i] == fullName)
+            {
+                // name exists, do nothing
+                return false;
+            }
+        }
+
+        names = Arrays.copyOf(names, names.length + 1);
+        names[names.length - 1] = fullName;
         return true;
     }
-
     /**
      * Searches the array trying to find
      * all names that has passed in first name. Returns a String array containing all
      * matches.
      */
     public static String[] findByFirstName(final String firstName){
+        String[] result = new String [0];
+        for (String name : names) {
+            String[] parts = name.split("");
+            if (parts[0].equals(firstName)) {
+                result = Arrays.copyOf(result, result.length +1);
+                result [result.length -1]  =name;
+            }
+        }
         return new String[0];
     }
 
     /**
-     * Should find a
-     * name and replace it with new fullName if available. Returns true if name was found and updated with
-     * the new name. False if name could not be updated because name was’t found or name was found but
-     * an existing name matching the updatedName already exists.
+     * Searches the array trying to find all names that has passed in last name. Returns a String array
+     * containing all
+     * matches.
      */
     public static String[] findByLastName(final String lastName){
+        String[] result = new String [0];
+        for (String name : names) {
+            String[] parts = name.split("");
+            if (parts[1]).equals(lastName)) {
+                result = Arrays.copyOf(result, result.length +1);
+                result[result.length -1] = name;
+            }
+        }
         return new String[0];
     }
 
@@ -71,8 +100,53 @@ public class NameRepository {
      * Should remove a name from the array.
      * Returns true if name was removed and false if the name was not removed for some reason
      */
-    public static boolean remove(final String fullName){
+    public static boolean remove(final String fullName) {
+
+        boolean success = false;
+
+        String[] copy = new String[names.length - 1];
+        int index = 0;
+        for (int i = 0; i < names.length; i++) {
+            if(names[i] == fullName)
+            {
+                success = true;
+                continue;
+            }
+            copy[index] = names[i];
+            index++;
+        }
+
+        names = copy;
+
+        return success;
+
+        /* for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(fullName)) {
+                names = removeFromArray(names, i);
+                return true;
+            }
+        }
+        */
+
+        //return false;
+    }
+
+    /**
+     * Should find a
+     * name and replace it with new fullName if available. Returns true if name was found and updated with
+     * the new name. False if name could not be updated because name wasn’t found or name was found but
+     * an existing name matching the updatedName already exists.
+     */
+    public static boolean update(final String original, final String updatedName){
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(original)) {
+                names[i] = updatedName;
+                return true;
+            }
+        }
         return false;
+
     }
 
 }
